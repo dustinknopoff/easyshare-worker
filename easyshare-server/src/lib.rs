44831b5f7096d400c8003b324f364203bb1cd900
Dustin Knopoff <dustin@knopoff.dev>
2023-11-20 10:32:33 -0500

    getting an existing share

--- easyshare-server/src/lib.rs
+++ easyshare-server/src/lib.rs
@@ -9,6 +9,8 @@ use crate::ui::layout;
 mod ui;
 mod utils;
 
+const WORKER_URL: &str = "http://localhost:8787";
+
 #[event(start)]
 pub fn start() {
     utils::set_panic_hook();
@@ -25,9 +27,97 @@ pub fn start() {
 }
 
 #[event(fetch, respond_with_errors)]
-async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
+async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     tracing::info!(request=?req, "Handling request");
-    Response::from_html(
-        layout::layout("EasyShare", html!("Hello World "(Uuid::new_v4()))).into_string(),
-    )
+
+    let router = Router::new();
+
+    router
+        .get("/", |_, _| {
+            Response::from_html(
+                layout::layout("EasyShare", html!("Hello World "(Uuid::new_v4()))).into_string(),
+            )
+        })
+        .get_async("/obj/:key", |_req, ctx| async move {
+            let Some(id) = ctx.param("key") else {
+                return Response::error("key required", 404);
+            };
+
+            let bucket = ctx.bucket("EASYSHARE_BUCKET")?;
+
+            let Some(object) = bucket.get(id).execute().await? else {
+                return Response::error("No object found", 404);
+            };
+
+            let r2_headers = object.http_metadata();
+            let headers = Headers::new();
+            let mut headers = write_http_headers(headers, r2_headers)?;
+            headers.set("etag", &object.http_etag())?;
+
+
+            Response::from_bytes(object.body().unwrap().bytes().await?)
+            .map(|req| req.with_headers(headers))
+        })
+        .get_async("/share/:id", |_req, ctx| async move {
+            let Some(id) = ctx.param("id") else {
+                return Response::error("ID required", 404);
+            };
+
+            let bucket = ctx.bucket("EASYSHARE_BUCKET")?;
+
+            let folder: Vec<String> = bucket
+                .list()
+                .prefix(id)
+                .execute()
+                .await?
+                .objects()
+                .iter()
+                .map(|object| format!("{WORKER_URL}/obj/{}", object.key()))
+                .collect();
+
+            Response::from_html(
+                layout::layout(
+                    "EasyShare",
+                    html!(
+                        @if folder.is_empty() {
+                            p {
+                                "No share found or is expired."
+                            }
+                        }
+                        ul {
+                            @for object in folder {
+                                li {
+                                    (object)
+                                }
+                            }
+                        }
+                    ),
+                )
+                .into_string(),
+            )
+        })
+        .run(req, env)
+        .await
+}
+
+fn write_http_headers(mut headers: Headers, r2_metadata: HttpMetadata) -> Result<Headers> {
+    if let Some(content_type) = r2_metadata.content_type {
+        headers.set("content-type", &content_type)?;
+    }
+    if let Some(content_language) = r2_metadata.content_language {
+        headers.set("content-language", &content_language)?;
+    }
+    if let Some(content_disposition) = r2_metadata.content_disposition {
+        headers.set("content-disposition", &content_disposition)?;
+    }
+    if let Some(content_encoding) = r2_metadata.content_encoding {
+        headers.set("content-encoding", &content_encoding)?;
+    }
+    if let Some(cache_control) = r2_metadata.cache_control {
+        headers.set("cache-control", &cache_control)?;
+    }
+    if let Some(cache_expiry) = r2_metadata.cache_expiry {
+        headers.set("cache-expiry", &format!("max-age={}", cache_expiry.as_millis()))?;
+    }
+    Ok(headers)
 }
